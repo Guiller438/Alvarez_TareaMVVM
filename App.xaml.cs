a2d042cc8d36702242f9089f890fe6846da05676
Guillermo Alvarez <guilleomg26.9@gmail.com>
2023-12-29 15:06:49 -0500

    Se agrego el proyecto

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Alvarez_TareaMVVM
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Alvarez_TareaMVVM
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
